ad2769a6957f84a8d647f9f681a2207a9b524801
13479776 <Hees@HeesdeMacBook-Pro.local>
2016-09-08 17:53:37 +0800

    statTarget

--- R/chose.driver.R
+++
@@ -1,8 +0,0 @@
-chose.driver <-
-function(scaling) {
-  pwd.n = paste(getwd(), "/Preprocessing_Data_", scaling, "/ProcessedTable.csv", sep="")
-  x <- read.csv(pwd.n, sep=",", header=TRUE)
-  x.x <- x[,2:ncol(x)]
-  rownames(x.x) <- x[,1]
-  print(colnames(x.x[1:10]))
-  }
--- R/zzz.R
+++
@@ -1,5 +0,0 @@
-.onAttach <- function(...) {
-  packageStartupMessage("\nUse 'statTarget.gui()' to restart the programe.\n",fill=TRUE)
-  #statTarget::statTargetGUI()
-  if (interactive()) statTarget.gui()
-}
